e52b803d2f489f5db610edd6f2df1991e0ae2f76
Egor Ivanov <rust_lang_lover@proton.me>
2024-12-27 16:09:37 +0300

    // checkpoint

---
+++ main.py
@@ -0,0 +1,8 @@
+def makeGood(s: str) -> str:
+    stack = []
+    for char in s:
+        if stack and abs(ord(stack[-1]) - ord(char)) == 32:
+            stack.pop()
+        else:
+            stack.append(char)
+    return "".join(stack)
---
+++ stacks_and_queues/queues.py
@@ -0,0 +1,20 @@
+# Declaration: we will use deque from the collections module
+import collections
+queue = collections.deque()
+
+# If you want to initialize it with some initial values:
+queue = collections.deque([1, 2, 3])
+
+# Enqueueing/adding elements:
+queue.append(4)
+queue.append(5)
+
+# Dequeuing/removing elements:
+queue.popleft() # 1
+queue.popleft() # 2
+
+# Check element at front of queue (next element to be removed)
+queue[0] # 3
+
+# Get size
+len(queue) # 3
