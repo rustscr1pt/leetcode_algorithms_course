79e1e8eb2967d438676d927d3840fea486892a61
Egor Ivanov <rust_lang_lover@proton.me>
2024-12-28 18:13:35 +0300

    pushed via Makefile

--- main.py
+++
@@ -1,8 +0,0 @@
-def makeGood(s: str) -> str:
-    stack = []
-    for char in s:
-        if stack and abs(ord(stack[-1]) - ord(char)) == 32:
-            stack.pop()
-        else:
-            stack.append(char)
-    return "".join(stack)
